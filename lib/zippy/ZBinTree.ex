ce62c80c465c716f6df8b631f0d6e67cf3a4ebdd
Théophile Choutri <theophile@choutri.eu>
2017-02-14 14:54:21 +0100

    fixed the functions

--- lib/zippy/ZBinTree.ex
+++ lib/zippy/ZBinTree.ex
@@ -20,6 +20,10 @@ defmodule Zippy.ZBinTree do
   @typedoc "A Zipper binary tree"
   @type  t() :: {thread(any()), node(any())}
 
+  # defimpl Enumerable, for: Tuple do
+
+  # end
+
   ## Functions
   
   @doc "Create a new basic binary tree. It should be declared first when declaring the data structure."
@@ -31,27 +35,27 @@ defmodule Zippy.ZBinTree do
   @doc "Checks if a node is a leaf, that is to say if it has no child."
   @spec leaf?(ZBinTree.t) :: boolean()
   def   leaf?({_thread, {:fork, _, nil, nil}}), do: true
-  def   leaf?({_thread, {:fork, _, _, _}}),       do: false
+  def   leaf?({_thread, {:fork, _, _, _}}),     do: false
 
   @doc "Returns the current element in the binary tree in a tuple."
   @spec current(ZBintTree.t) :: {:ok, term()} | {:error, nil}
   def   current({_thread, {:fork, value, _left, _right}}), do: {:ok, value}
-  def   current({_thread, nil}),                          do: {:error, nil}
+  def   current({_thread, nil}),                           do: {:error, nil}
 
   @doc "Replaces the current element in the tree (if it exists) or create a new node (if it doesn't)."
   @spec replace(ZBinTree.t, term()) :: ZBinTree.t
-  def   replace(value, {thread, nil}),                            do: {thread, {:fork, value, nil, nil}}
-  def   replace(value, {thread, {:fork, _oldvalue, left, right}}), do: {thread, {:fork, value, left, right}}
+  def   replace({thread, nil}, value),                             do: {thread, {:fork, value, nil, nil}}
+  def   replace({thread, {:fork, _oldvalue, left, right}}, value), do: {thread, {:fork, value, left, right}}
 
   @doc "Goes down the tree, with the `current` element being the `right` child."
   @spec right(ZBinTree.t) :: ZBinTree.t | nil
   def   right({thread, {:fork, value, left, right}}), do: {[{:right, value, left}|thread], right}
-  def   right({_thread, nil}),                       do: nil
+  def   right({_thread, nil}),                        do: nil
 
   @doc "Goes down the tree, with the `current` element being the `left` child, or returns `nil` if there is no child"
   @spec left(ZBinTree.t) :: ZBinTree.t | nil
   def   left({thread, {:fork, value, left, right}}), do: {[{:left, value, right}|thread], left}
-  def   left({_thread, nil}),                          do: nil
+  def   left({_thread, nil}),                        do: nil
 
   @doc "Goes up the tree, or returns `nil` if we're already at the top of the tree."
   @spec up(ZBinTree.t) :: ZBinTree | nil
@@ -61,16 +65,18 @@ defmodule Zippy.ZBinTree do
 
   @doc "Adds a left child to the tree"
   @spec add_left(ZBinTree.t, term()) :: ZBinTree.t
-  def   add_left(new_branch, zipper) do
-    new_branch
-    |> replace(left(zipper))
+  def   add_left(zipper, new_branch) do
+    zipper
+    |> left
+    |> replace(new_branch)
     |> up
   end
 
   @spec add_right(ZBinTree.t, term()) :: ZBinTree.t
-  def   add_right(new_branch, zipper) do
-    new_branch
-    |> replace(right(zipper))
+  def   add_right(zipper, new_branch) do
+    zipper
+    |> left
+    |> replace(new_branch)
     |> up
   end
 end
